98a6ce10e1ee89c6966ccaa863a74b6eee70a76c
Abdu11ahahmed <sjsuper872@gmail.com>
2026-01-23 22:41:29 +0200

    setting up the new project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// loader
+let loader = document.getElementById("loader");
+
+window.addEventListener("load", () => {
+    loader.classList.remove("active");
+    document.body.classList.add("active");
+});
+// loader
