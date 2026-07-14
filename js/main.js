5531f4034b3ddaf13ea265370e8c0318b5887cd5
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2026-07-15 01:53:06 +0300

    new sounds

--- js/main.js
+++ js/main.js
@@ -9,22 +9,29 @@ window.addEventListener("load", () => {
     let internet = document.querySelector(".internet");
     let interMessage = document.querySelector(".internet p");
     let internetRefresh = document.querySelector(".internet a");
+    let intro = new Audio(),
+        onlineAudio = new Audio(),
+        offlineAudio = new Audio();
+    intro.src = "../assets/best-seller/sounds/intro.mp3";
+    onlineAudio.src = "../assets/best-seller/sounds/core.mp3";
+    onlineAudio.src = "../assets/best-seller/sounds/core.mp3";
+    intro.play();
 
     window.addEventListener("offline", () => {
         internet.style.bottom = "20px";
         interMessage.textContent = "you lost the internet";
         internetRefresh.style.display = "flex";
         internet.style.backgroundColor = "#ff000033";
-        console.log("offline");
+        offlineAudio.play();
     });
     window.addEventListener("online", () => {
         interMessage.textContent = "keep Going Internet Restored";
         internetRefresh.style.display = "none";
         internet.style.backgroundColor = "#00ff2a28";
+        onlineAudio.play();
 
         setTimeout(() => {
             internet.style.bottom = "-50px";
-            internet.style.transition= " 1s bottom cubic-bezier(0.075, 0.82, 0.165, 1)"
         }, 5000);
     });
 
